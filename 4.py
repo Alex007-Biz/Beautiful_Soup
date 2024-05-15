9a114b5f4dd7dfa96f764f8f071f42dbd135c4a5
Alex007-Biz <bizukin@yandex.ru>
2024-05-15 08:17:43 +0300

    3, 4

---
+++ 4.py
@@ -0,0 +1,15 @@
+import requests
+from bs4 import BeautifulSoup
+
+def get_english_words():
+    url = "https://plitkanadom.ru/"
+    response = requests.get(url)
+    #print(response.text)
+    soup = BeautifulSoup(response.content, "html.parser")
+    english_words = soup.find("img", alt="Техническая пробка")
+
+
+    print(english_words)
+
+
+get_english_words()
